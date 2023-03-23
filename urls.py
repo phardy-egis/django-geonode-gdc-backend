4c36a40a7b62f49681f10d29a9e5c8b33293f364
p.hardy <pierre.hardy@egis-group.com>
2023-03-23 09:09:34 +0100

    Removed URL pointing to old version of django-geonode-gdc-frontend

--- urls.py
+++ urls.py
@@ -8,7 +8,6 @@ router = routers.DefaultRouter()
 router.register(r'resource_detail', views.ResourceDetailCustomViewSet)
 
 urlpatterns = [
-    path(r'', TemplateView.as_view(template_name='gdc.html')),
     path('api/', include(router.urls)),
     path(r'api/resource_list/', views.ResourceCustomListView.as_view()),
 ]
